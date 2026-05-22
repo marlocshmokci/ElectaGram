43fe6a8db518a2a9fa57c1fb96c7aa2a5a5776d8
tehcneko <chsqwyx@gmail.com>
2026-05-22 22:30:10 +0800

    Fix hiding stokes

--- TMessagesProj/src/main/java/org/telegram/ui/Components/blur3/drawable/BlurredBackgroundDrawable.java
+++ TMessagesProj/src/main/java/org/telegram/ui/Components/blur3/drawable/BlurredBackgroundDrawable.java
@@ -597,7 +597,7 @@ public abstract class BlurredBackgroundDrawable extends Drawable {
             return;
         }
 
-        final NinePatchDrawable ninePatchDrawable = checkNinePatchDrawable(fillColor, true);
+        final NinePatchDrawable ninePatchDrawable = checkNinePatchDrawable(fillColor, NekoConfig.strokeOnViews);
         ninePatchDrawable.setBounds(
             boundProps.boundsWithPadding.left - ninePatchDrawablePadding.left,
             boundProps.boundsWithPadding.top - ninePatchDrawablePadding.top,
